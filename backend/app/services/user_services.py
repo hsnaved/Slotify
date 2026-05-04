d6f4461ad449926db7809f29f430eb48c6291547
saidevansh <saidevansh023@gmail.com>
2026-05-04 22:00:15 +0530

    Devansh: Added Security and hashing the password mechanism using bcrypt

--- backend/app/services/user_services.py
+++ backend/app/services/user_services.py
@@ -4,13 +4,14 @@ from fastapi import HTTPException
 
 from app.schemas.user import UserCreate
 from app.models.user import User
+from app.core.security import hash_password
 
 def create_user_services(user_data:UserCreate, db:Session):
     user = User(
         username = user_data.username,
         email = user_data.email,
         number = user_data.number,
-        hashed_password = user_data.hashed_password,
+        hashed_password = hash_password(user_data.hashed_password),
         role = user_data.role
     )
 
