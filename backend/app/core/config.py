f429ceeb8f2f52096e83178ab9712278ca1fdd4c
saidevansh <saidevansh023@gmail.com>
2026-07-01 22:06:23 +0530

    Devansh- Modified the url

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,17 +1,34 @@
-from pydantic_settings import BaseSettings
+# from pydantic_settings import BaseSettings
+
+# class Settings(BaseSettings):
+#     """Application settings loaded from environment variables or the local .env file."""
+
+#     SECRET_KEY: str = "SECRET_KEY"
+#     ALGORITHM: str = "HS256"
+#     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60
+
+#     # DATABASE_URL: str = "postgresql://postgres:jamundi@localhost:5432/slotify_db"
+#     DATABASE_URL: str = "postgresql://neondb_owner:npg_HSnZ6RtaFb0Q@ep-icy-star-at2g8l2p-pooler.c-9.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
+
+#     class Config:
+#         env_file = ".env"
+        
+
+# settings = Settings()
+
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
 
 class Settings(BaseSettings):
-    """Application settings loaded from environment variables or the local .env file."""
-
-    SECRET_KEY: str = "SECRET_KEY"
+    SECRET_KEY: str
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60
+    DATABASE_URL: str
 
-    # DATABASE_URL: str = "postgresql://postgres:jamundi@localhost:5432/slotify_db"
-    DATABASE_URL: str = "postgresql://neondb_owner:npg_HSnZ6RtaFb0Q@ep-icy-star-at2g8l2p-pooler.c-9.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        extra="ignore"
+    )
 
-    class Config:
-        env_file = ".env"
-        
 
 settings = Settings()
